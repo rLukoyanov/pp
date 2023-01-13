7c8971f50c4c5ad1410e8732d781169173024a31
rLukoyanov <rulukoyanov@gmail.com>
2023-01-13 16:26:50 +0300

    feat(20): add popup into achievements block

--- src/components/Profile/Achievements/AddAchieve/Component.tsx
+++ src/components/Profile/Achievements/AddAchieve/Component.tsx
@@ -1,41 +1,31 @@
 import cn from 'classnames';
+
 import { Button } from '../../../common/Button';
-import { Input } from '../../../common/Input';
 import { TextArea } from '../../../common/TextArea';
 
 import crossSVG from './cross.svg';
 
 import styles from './styles.module.scss';
 
-export function AddCard({
+export function AddAchieve({
   onClick,
 }: {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }) {
   return (
-    <div className={cn(styles.card)}>
+    <div className={cn(styles.addAchieve)}>
       <div className={cn(styles.content)}>
         <div className={cn(styles.header)}>
-          <h2>Опыт работы</h2>
+          <h2>Краткое описание достижения</h2>
 
           <button className={cn(styles.close)} onClick={onClick}>
             <img src={crossSVG} alt="close button" />
           </button>
         </div>
 
-        <label>
-          <h3>Должность</h3>
-          <Input classNames={cn(styles.input)} />
-        </label>
-        <label>
-          <h3>Место работы</h3>
-          <Input classNames={cn(styles.input)} />
-        </label>
-        <label>
-          <h3>Детали</h3>
-          <TextArea />
-        </label>
+        <TextArea />
         <br />
+        <Button className={cn(styles.upload)}>Загрузить фото</Button>
         <Button className={cn(styles.done)}>Разместить</Button>
       </div>
     </div>
--- src/components/Profile/Achievements/Component.tsx
+++ src/components/Profile/Achievements/Component.tsx
@@ -6,13 +6,24 @@ import { Button } from '../../common/Button';
 import { Achieve } from './Achieve';
 
 import styles from './styles.module.scss';
+import { useState } from 'react';
+import { AddAchieve } from './AddAchieve';
 
 export function Achievements() {
+  const [addAchieveOpen, setAddAchieveOpen] = useState(false);
+  const addAchieve = () => {
+    setAddAchieveOpen((prev) => !prev);
+  };
+  const closeAchieve = () => {
+    setAddAchieveOpen(false);
+  };
+
   return (
     <div className={cn(styles.achievements)}>
+      {addAchieveOpen && <AddAchieve onClick={closeAchieve} />}
       <div className={cn(styles.title)}>
         <h1>Достижения</h1>
-        <Button type="outline">
+        <Button onClick={addAchieve} type="outline">
           <img src={plusSVG} alt="buttonImg" />
         </Button>
       </div>
